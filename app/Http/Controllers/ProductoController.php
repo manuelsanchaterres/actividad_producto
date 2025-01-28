f1af2ecef733afffb0f83e476b6df51bedea9626
manuelsanchaterres <manuelsanchaterres@gmail.com>
2025-01-28 20:40:12 +0100

    Actividad 23 Finalizada

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    public function index(){
+        $productos = [
+            ['nombre' => 'Laptop Gamer', 'precio' => 1200, 'descripcion' => 'Laptop para gaming.'],
+            ['nombre' => 'Teclado Mecánico', 'precio' => 100, 'descripcion' => 'Teclado RGB con switches mecánicos.'],
+            ['nombre' => 'Mouse Inalámbrico', 'precio' => 50, 'descripcion' => 'Mouse ergonómico y recargable.'],
+            ['nombre' => 'EarPods', 'precio' => 14.95, 'descripcion' => 'Auriculares Inalámbricos Económicos'],
+        ];
+		return view('productos.index',['productos' => $productos]);
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/productos', function () {
-
-    $productos = [
-        ['nombre' => 'Laptop Gamer', 'precio' => 1200, 'descripcion' => 'Laptop para gaming.'],
-        ['nombre' => 'Teclado Mecánico', 'precio' => 100, 'descripcion' => 'Teclado RGB con switches mecánicos.'],
-        ['nombre' => 'Mouse Inalámbrico', 'precio' => 50, 'descripcion' => 'Mouse ergonómico y recargable.'],
-        ['nombre' => 'EarPods', 'precio' => 14.95, 'descripcion' => 'Auriculares Inalámbricos Económicos'],
-    ];
-    return view('productos.index',['productos' => $productos]);
-});
+Route::get('/productos', [ProductoController::class,'index']);
 
